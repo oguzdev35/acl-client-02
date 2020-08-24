129178a9a33b81f886e7115b21ce450c0a869b27
dogusdicle <dogus.dicle@maksisoft.com>
2020-08-24 12:41:51 +0300

    ed

---
+++ precache-manifest.deb96b1b8965dc0be58c8e89a741e2f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4914859bc9c827727cc945b8b1c3b496",
+    "url": "/index.html"
+  },
+  {
+    "revision": "347723408bdeeda0766c",
+    "url": "/static/css/2.3a98e74b.chunk.css"
+  },
+  {
+    "revision": "130c0bcd0c73c5c5297d",
+    "url": "/static/css/main.98b19b36.chunk.css"
+  },
+  {
+    "revision": "347723408bdeeda0766c",
+    "url": "/static/js/2.ed930c4b.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.ed930c4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "130c0bcd0c73c5c5297d",
+    "url": "/static/js/main.177910b8.chunk.js"
+  },
+  {
+    "revision": "f8e80ea9a0c36bf28a24",
+    "url": "/static/js/runtime-main.0308fc18.js"
+  }
+]);
